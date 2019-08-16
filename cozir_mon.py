e7b5075bfca6dead9a8298805e4d3bebb0357654
Erik Tollerud <erik.tollerud@gmail.com>
2019-08-16 10:29:13 -0400

    add loop

--- cozir_mon.py
+++ cozir_mon.py
@@ -1,4 +1,6 @@
+from time import sleep
 import serial
+from datetime import datetime
 
 def setup_connection(port):
     ser = serial.Serial(port, timeout=1)
@@ -8,19 +10,44 @@ def setup_connection(port):
 
     return serial.Serial(port, timeout=1)
 
-def get_single_measurement(ser):
-    ser.write(b'Q\r\n')
-    return ser.read(200)
+def get_single_measurement(ser, n_samples=1):
+    lines = []
+    for _ in range(n_samples):
+        ser.write(b'Q\r\n')
+        lines.append(ser.read(200))
+
+    return b''.join(lines)
+
+
+def do_measurement_loop(ser, fn, delay, n_samples):
+    with open(fn, 'w') as f:
+        while True:
+            f.write(str(datetime.now()))
+            meas = get_single_measurement(ser, n_samples)
+            f.write(meas)
+            f.write(str(datetime.now()))
+
+            f.flush()
+
+            sleep(delay)
 
 if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('output_file')
+    parser.add_argument('--output-file', '-o', default=None)
     parser.add_argument('--port', '-p', default='/dev/serial0')
+    parser.add_argument('--delay', '-d', default=60)
+    parser.add_argument('--number-samples', '-n', default=5)
 
     args = parser.parse_args()
 
+
     ser = setup_connection(args.port)
-    print(get_single_measurement(ser))
+    try:
+        if args.output_file is None:
+            print(get_single_measurement(ser))
+        else:
+            do_measurement_loop(ser, args.output_file, args.delay, args.number_samples)
+    finally:
     ser.close()
