e30074841636f17cfd1954482f5709c2863e8180
Erik Tollerud <erik.tollerud@gmail.com>
2019-09-30 19:57:00 -0400

    get bme calibrator working

--- feather/cozir_parser.py
+++ feather/cozir_parser.py
@@ -62,13 +62,26 @@ class BME280_calibrator:
     Note that temp *must* be calculated before pressure/humidity to set the t_fine
     correctly
     """
-    def __init__(self, calibcoeffs):
-        self.t_calib = calibcoeffs[0]
-        self.p_calib = calibcoeffs[1]
-        self.h_calib = calibcoeffs[2]
-
-
+    def __init__(self, t_calib, p_calib, h_calib):
+        self.t_calib = t_calib
+        self.p_calib = p_calib
+        self.h_calib = h_calib
         self.t_fine = None
+
+    @classmethod
+    def coeffs_from_table(cls, tab, which=-1):
+        calib_meas_names = [(t, t[-3], int(t[-1])) for t in
+                            np.unique(tab['measurement_type']) if
+                            t.startswith('bme280_calib')]
+
+        calib_coeffs = []
+        for calib_type in ['t', 'p', 'h']:
+            calib_num_to_name = {i: nm for nm,t,i in calib_meas_names if
+                                           t == calib_type}
+            calib_meas_name = [calib_num_to_name[k] for k in sorted(calib_num_to_name.keys())]
+            calib_coeffs.append([tab[tab['measurement_type']==n][which]['value'] for n in calib_meas_name])
+
+        return cls(*calib_coeffs)
 
 
     def _set_t_fine(self, traw):
@@ -76,14 +89,14 @@ class BME280_calibrator:
         var2 = ((traw/131072.0 - (self.t_calib[0])/8192.0) * (traw/131072.0 - (self.t_calib[0])/8192.0)) * (self.t_calib[2])
         self.t_fine = var1 + var2
 
-    def calib_temp(self, traw):
+    def calibrate_temp(self, traw):
         """
         Returns temperature in deg C
         """
         self._set_t_fine(traw)
         return np.clip(self.t_fine / 5120.0, -273.15, None)
 
-    def calib_pressure(self, praw):
+    def calibrate_pressure(self, praw):
         """
         Returns pressure in Pa
         """
@@ -94,14 +107,14 @@ class BME280_calibrator:
         var1 = (self.p_calib[2] * var1 * var1 / 524288.0 + self.p_calib[1] * var1) / 524288.0
         var1 = (1.0 + var1 / 32768.0)*self.p_calib[0]
 
-        p = 1048576.0 - float(praw)
+        p = 1048576.0 - praw
         p = (p - (var2 / 4096.0)) * 6250.0 / var1
         var1 = (self.p_calib[8]) * p * p / 2147483648.0
         var2 = p * (self.p_calib[7]) / 32768.0
         p = p + (var1 + var2 + (self.p_calib[6])) / 16.0
         return np.clip(p, 0, None)
 
-    def calib_humidity(self, hraw):
+    def calibrate_humidity(self, hraw):
         """
         Returns humidity in RH (%)
         """
@@ -110,3 +123,34 @@ class BME280_calibrator:
         var_H = var_H * (1.0 - self.h_calib[0] * var_H / 524288.0)
 
         return np.clip(var_H, 0, 100)
+
+    def calibrate_table(self, tab, measurement_types={'temp(C)': 'bme280_temp_raw',
+                                                      'pressure(Pa)': 'bme280_pressure_raw',
+                                                      'humidity(%)': 'bme280_humidity_raw'}):
+        calibed = {}
+        # temp must go first...
+        measurement_keys = list(measurement_types.keys())
+        measurement_keys.remove('temp(C)')
+        measurement_keys.insert(0, 'temp(C)')
+
+        for nm in measurement_keys:
+            mtype = measurement_types[nm]
+
+            rawvals = tab[tab['measurement_type'] == mtype]['value']
+            if nm == 'temp(C)':
+                calibvals = self.calibrate_temp(rawvals)
+            elif nm == 'pressure(Pa)':
+                calibvals = self.calibrate_pressure(rawvals)
+            elif nm == 'humidity(%)':
+                calibvals = self.calibrate_humidity(rawvals)
+            else:
+                raise ValueError(f'unrecognized measurement result {nm}')
+
+            calibed[nm] = calibvals
+
+        return calibed
+
+
+
+
+#traw = tab[tab['measurement_type']=='bme280_temp_raw']['value']
